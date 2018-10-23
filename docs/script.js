b39d91ae601de5df43c16583aab964478c660f09
sumi <30201827+46ra@users.noreply.github.com>
2018-10-23 00:30:10 -0700

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -9,40 +9,130 @@ const pos = {
 
 const charToPaths = {
   " ": [],
-  ".": [[[4, 3], [4, 4]]],
-  ",": [[[4, 3], [3, 4]]],
-  "-": [[pos.cl, pos.cr]],
-  "'": [[pos.t, [3, 1]]],
-  "`": [[pos.t, [4, 1]]],
-  ":": [[pos.t, [4, 1]], [[4, 3], pos.br]],
-  ";": [[pos.t, [4, 1]], [[4, 3], [3, 4]]],
-  a: [[pos.bl, pos.t, pos.br], [pos.cl, pos.cr]],
-  b: [[pos.bl, pos.t, pos.br, pos.bl], [pos.cl, pos.cr]],
-  c: [[pos.cr, pos.t, pos.bl, pos.br]],
-  d: [[pos.t, pos.br, pos.bl, pos.cl, pos.cr]],
-  e: [[pos.t, pos.bl, pos.br], [pos.cl, pos.cr]],
-  f: [[pos.cr, pos.t, pos.bl], [pos.cl, pos.br]],
-  g: [[pos.t, pos.bl, pos.br, pos.cr]],
-  h: [[pos.t, pos.bl], [pos.cl, pos.cr, pos.br]],
-  i: [[pos.t, pos.cl, pos.bc], [pos.bl, pos.br]],
-  j: [[pos.cl, pos.t, pos.br, pos.bl]],
-  k: [[pos.t, pos.bl], [pos.cr, pos.cl, pos.br]],
-  l: [[pos.t, pos.bl, pos.br]],
-  m: [[pos.bl, pos.t, pos.br], [pos.cr, pos.bc]],
-  n: [[pos.bl, pos.bc, pos.cl, pos.br, pos.t]],
-  o: [[pos.t, pos.bl, pos.br, pos.t]],
-  p: [[pos.bl, pos.t, pos.br, pos.cl]],
-  q: [[pos.bc, pos.cr, pos.t, pos.bl, pos.br]],
-  r: [[pos.bl, pos.t, pos.cr, pos.cl, pos.br]],
-  s: [[pos.t, pos.cl, pos.cr, pos.br, pos.bl]],
-  t: [[pos.bl, pos.t], [pos.cl, pos.br]],
+  ".": [
+    [
+      [4, 3],
+      [4, 4]
+    ]
+  ],
+  ",": [
+    [
+      [4, 3],
+      [3, 4]
+    ]
+  ],
+  "-": [
+    [pos.cl, pos.cr]
+  ],
+  "'": [
+    [pos.t, [3, 1]]
+  ],
+  "`": [
+    [pos.t, [4, 1]]
+  ],
+  ":": [
+    [pos.t, [4, 1]],
+    [
+      [4, 3], pos.br
+    ]
+  ],
+  ";": [
+    [pos.t, [4, 1]],
+    [
+      [4, 3],
+      [3, 4]
+    ]
+  ],
+  a: [
+    [pos.bl, pos.t, pos.br],
+    [pos.cl, pos.cr]
+  ],
+  b: [
+    [pos.bl, pos.t, pos.br, pos.bl],
+    [pos.cl, pos.cr]
+  ],
+  c: [
+    [pos.cr, pos.t, pos.bl, pos.br]
+  ],
+  d: [
+    [pos.t, pos.br, pos.bl, pos.cl, pos.cr]
+  ],
+  e: [
+    [pos.t, pos.bl, pos.br],
+    [pos.cl, pos.cr]
+  ],
+  f: [
+    [pos.cr, pos.t, pos.bl],
+    [pos.cl, pos.br]
+  ],
+  g: [
+    [pos.t, pos.bl, pos.br, pos.cr]
+  ],
+  h: [
+    [pos.t, pos.bl],
+    [pos.cl, pos.cr, pos.br]
+  ],
+  i: [
+    [pos.t, pos.cl, pos.bc],
+    [pos.bl, pos.br]
+  ],
+  j: [
+    [pos.cl, pos.t, pos.br, pos.bl]
+  ],
+  k: [
+    [pos.t, pos.bl],
+    [pos.cr, pos.cl, pos.br]
+  ],
+  l: [
+    [pos.t, pos.bl, pos.br]
+  ],
+  m: [
+    [pos.bl, pos.t, pos.br],
+    [pos.cr, pos.bc]
+  ],
+  n: [
+    [pos.bl, pos.bc, pos.cl, pos.br, pos.t]
+  ],
+  o: [
+    [pos.t, pos.bl, pos.br, pos.t]
+  ],
+  p: [
+    [pos.bl, pos.t, pos.br, pos.cl]
+  ],
+  q: [
+    [pos.bc, pos.cr, pos.t, pos.bl, pos.br]
+  ],
+  r: [
+    [pos.bl, pos.t, pos.cr, pos.cl, pos.br]
+  ],
+  s: [
+    [pos.t, pos.cl, pos.cr, pos.br, pos.bl]
+  ],
+  t: [
+    [pos.bl, pos.t],
+    [pos.cl, pos.br]
+  ],
   // u: [[pos.bl, pos.cl, pos.bc, pos.br, pos.t]],
-  u: [[pos.cl, pos.bl, pos.br, pos.t]],
-  v: [[pos.bl, pos.cl, pos.br, pos.t]],
-  w: [[pos.bl, [1, 3], pos.bc, pos.cl, pos.br, pos.t]],
-  x: [[pos.bl, pos.cl, pos.br], [pos.t, pos.cr, pos.bc]],
-  y: [[pos.bl, pos.cl, pos.br], [pos.t, pos.br, pos.bc]],
-  z: [[pos.bl, pos.t, pos.cr, pos.bc, pos.br]]
+  u: [
+    [pos.cl, pos.bl, pos.br, pos.t]
+  ],
+  v: [
+    [pos.bl, pos.cl, pos.br, pos.t]
+  ],
+  w: [
+    [pos.bl, [1, 3], pos.bc, pos.cl, pos.br, pos.t]
+  ],
+  x: [
+    [pos.bl, pos.cl, pos.br],
+    [pos.t, pos.cr, pos.bc]
+  ],
+  y: [
+    [pos.bl, pos.cl, pos.br],
+    [pos.t, pos.br, pos.bc]
+  ],
+  z: [
+    [pos.bl, pos.t, pos.cr, pos.bc, pos.br]
+  ]
 };
 
 const unit = 4;
@@ -55,41 +145,40 @@ document.addEventListener("DOMContentLoaded", event => {
   }
 
   for (const [f, id] of [
-    [x => x, "input"],
-    [parseInt, "letter-w"],
-    [parseInt, "line-w"],
-    [parseFloat, "space-x"],
-    [parseFloat, "space-y"],
-    [parseFloat, "margin"],
-    [x => x, "color"],
-    [x => x, "bg-color"]
-  ])
+      [x => x, "input"],
+      [parseInt, "letter-w"],
+      [parseInt, "line-w"],
+      [parseFloat, "space-x"],
+      [parseFloat, "space-y"],
+      [parseFloat, "margin"],
+      [x => x, "color"],
+      [x => x, "bg-color"]
+    ])
     if (query[id]) document.getElementById(id).value = f(query[id]);
 
   render();
 
   for (const element of document.getElementsByClassName("trigger"))
-    element.addEventListener("input", () => {
-      render();
+    element.addEventListener("input", render);
 
-      const queryString =
-        "?" +
-        [
-          "letter-w",
-          "line-w",
-          "space-x",
-          "space-y",
-          "margin",
-          "color",
-          "bg-color",
-          "input"
-        ]
-          .map(id => id + "=" + document.getElementById(id).value)
-          .join("&");
+  document.getElementById("update-url").addEventListener("click", () => {
+    const queryString =
+      "?" + [
+        "letter-w",
+        "line-w",
+        "space-x",
+        "space-y",
+        "margin",
+        "color",
+        "bg-color",
+        "input"
+      ]
+      .map(id => id + "=" + document.getElementById(id).value)
+      .join("&");
 
-      window.location.search =
-        window.location.search.split("?")[0] + queryString;
-    });
+    window.location.search =
+      window.location.search.split("?")[0] + queryString;
+  })
 });
 
 const render = () => {
